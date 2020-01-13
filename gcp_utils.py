7537acbde6fcf2dd8962fac75550d240c3b89b53
Kathryn Jin <kjkjkathryn@gmail.com>
2020-01-13 17:33:39 -0500

    ingestion unzip folder debugging

--- gcp_utils.py
+++ gcp_utils.py
@@ -5,7 +5,7 @@ from google.cloud import storage
 
 def list_files(gcp_bucket_name, prefix):
     gcp_key_file_location = next(Path('./keys').iterdir())
-    storage_client = storage.Client.from_service_account_json(gcp_key_file_location.as_posix())
+    storage_client = storage.Client.from_service_account_json(gcp_key_file_location)
     bucket_name = gcp_bucket_name
     bucket = storage_client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=prefix, max_results=10000)
