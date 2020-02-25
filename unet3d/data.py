1745ec6fb7aa9873f63a42d4374e166aafae5005
kjin <kjin@mit.edu>
2020-02-25 10:51:54 -0500

    tables debugging

--- unet3d/data.py
+++ unet3d/data.py
@@ -33,7 +33,7 @@ def write_image_data_to_file(image_files, data_storage, truth_storage, image_sha
 
 
 def add_data_to_storage(data_storage, truth_storage, subject_data, n_channels, truth_dtype):
-    data_storage.append(np.asarray(subject_data[:n_channels])[np.newaxis])
+    data_storage.append(np.asarray(subject_data[:n_channels]))
     truth_storage.append(np.asarray(subject_data[n_channels], dtype=truth_dtype)[np.newaxis][np.newaxis])
     # affine_storage.append(np.asarray(affine)[np.newaxis])
 
