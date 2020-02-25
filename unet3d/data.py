4c464403d2698b88bb4e2ccdf1691b1e83465f6f
kjin <kjin@mit.edu>
2020-02-25 11:02:50 -0500

    tables debugging

--- unet3d/data.py
+++ unet3d/data.py
@@ -10,8 +10,8 @@ from .normalize import normalize_data_storage, reslice_image_set
 def create_data_file(out_file, n_channels, n_samples, image_shape):
     hdf5_file = tables.open_file(out_file, mode='w')
     filters = tables.Filters(complevel=5, complib='blosc')
-    data_shape = tuple([0] + list(image_shape))
-    truth_shape = tuple([0] + list(image_shape))
+    data_shape = tuple([0, n_channels] + list(image_shape))
+    truth_shape = tuple([0, 1] + list(image_shape))
     sys.stdout.write("*************" + str(data_shape))
     data_storage = hdf5_file.create_earray(hdf5_file.root, 'data', tables.Float32Atom(), shape=data_shape,
                                            filters=filters, expectedrows=n_samples)
@@ -33,6 +33,7 @@ def write_image_data_to_file(image_files, data_storage, truth_storage, image_sha
 
 
 def add_data_to_storage(data_storage, truth_storage, subject_data, n_channels, truth_dtype):
+    sys.stdout.write(np.asarray(subject_data[:n_channels])[np.newaxis].shape)
     data_storage.append(np.asarray(subject_data[:n_channels])[np.newaxis])
     truth_storage.append(np.asarray(subject_data[n_channels], dtype=truth_dtype)[np.newaxis][np.newaxis])
     # affine_storage.append(np.asarray(affine)[np.newaxis])
@@ -43,13 +44,13 @@ def write_data_to_file(training_data_files, out_file, image_shape, truth_dtype=n
     """
     Takes in a set of training images and writes those images to an hdf5 file.
     :param training_data_files: List of tuples containing the training data files. The modalities should be listed in
-    the same order in each tuple. The last item in each tuple must be the labeled image. 
-    Example: [('sub1-T1.nii.gz', 'sub1-T2.nii.gz', 'sub1-truth.nii.gz'), 
+    the same order in each tuple. The last item in each tuple must be the labeled image.
+    Example: [('sub1-T1.nii.gz', 'sub1-T2.nii.gz', 'sub1-truth.nii.gz'),
               ('sub2-T1.nii.gz', 'sub2-T2.nii.gz', 'sub2-truth.nii.gz')]
     :param out_file: Where the hdf5 file will be written to.
     :param image_shape: Shape of the images that will be saved to the hdf5 file.
-    :param truth_dtype: Default is 8-bit unsigned integer. 
-    :return: Location of the hdf5 file with the image data written to it. 
+    :param truth_dtype: Default is 8-bit unsigned integer.
+    :return: Location of the hdf5 file with the image data written to it.
     """
     n_samples = len(training_data_files)
     n_channels = len(training_data_files[0]) - 1
