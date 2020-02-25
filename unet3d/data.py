f8af5ff40c464d510afb521ee556b5b65e1bca92
kjin <kjin@mit.edu>
2020-02-25 11:55:08 -0500

    revert a bad idea

--- unet3d/data.py
+++ unet3d/data.py
@@ -33,8 +33,8 @@ def write_image_data_to_file(image_files, data_storage, truth_storage, image_sha
 
 
 def add_data_to_storage(data_storage, truth_storage, subject_data, n_channels, truth_dtype):
-    sys.stdout.write(str(np.asarray(subject_data[:n_channels])[np.newaxis][np.newaxis].shape))
-    data_storage.append(np.asarray(subject_data[:n_channels])[np.newaxis][np.newaxis])
+    sys.stdout.write(str(np.asarray(subject_data[:n_channels])[np.newaxis].shape))
+    data_storage.append(np.asarray(subject_data[:n_channels])[np.newaxis])
     truth_storage.append(np.asarray(subject_data[n_channels], dtype=truth_dtype)[np.newaxis][np.newaxis])
     # affine_storage.append(np.asarray(affine)[np.newaxis])
 
