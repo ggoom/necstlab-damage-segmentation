38d56e9db92a1c816fdb2f3ee5c063d57bce7fe8
kjin <kjin@mit.edu>
2020-03-10 10:12:15 -0400

    comment out all nilearn

--- unet3d/augment.py
+++ unet3d/augment.py
@@ -5,22 +5,22 @@ import random
 import itertools
 
 
-# def scale_image(image, scale_factor):
-#     scale_factor = np.asarray(scale_factor)
-#     new_affine = np.copy(image.affine)
-#     new_affine[:3, :3] = image.affine[:3, :3] * scale_factor
-#     new_affine[:, 3][:3] = image.affine[:, 3][:3] + (image.shape * np.diag(image.affine)[:3] * (1 - scale_factor)) / 2
-#     return new_img_like(image, data=image, affine=new_affine)
+def scale_image(image, scale_factor):
+    scale_factor = np.asarray(scale_factor)
+    new_affine = np.copy(image.affine)
+    new_affine[:3, :3] = image.affine[:3, :3] * scale_factor
+    new_affine[:, 3][:3] = image.affine[:, 3][:3] + (image.shape * np.diag(image.affine)[:3] * (1 - scale_factor)) / 2
+    return new_img_like(image, data=image, affine=new_affine)
 
 
-# def flip_image(image, axis):
-#     try:
-#         new_data = np.copy(image)
-#         for axis_index in axis:
-#             new_data = np.flip(new_data, axis=axis_index)
-#     except TypeError:
-#         new_data = np.flip(image, axis=axis)
-#     return new_img_like(image, data=new_data)
+def flip_image(image, axis):
+    try:
+        new_data = np.copy(image)
+        for axis_index in axis:
+            new_data = np.flip(new_data, axis=axis_index)
+    except TypeError:
+        new_data = np.flip(image, axis=axis)
+    return new_img_like(image, data=new_data)
 
 
 def random_flip_dimensions(n_dimensions):
@@ -39,35 +39,35 @@ def random_boolean():
     return np.random.choice([True, False])
 
 
-# def distort_image(image, flip_axis=None, scale_factor=None):
-#     if flip_axis:
-#         image = flip_image(image, flip_axis)
-#     if scale_factor is not None:
-#         image = scale_image(image, scale_factor)
-#     return image
+def distort_image(image, flip_axis=None, scale_factor=None):
+    if flip_axis:
+        image = flip_image(image, flip_axis)
+    if scale_factor is not None:
+        image = scale_image(image, scale_factor)
+    return image
 
 
-# def augment_data(data, truth, affine, scale_deviation=None, flip=True):
-#     n_dim = len(truth.shape)
-#     if scale_deviation:
-#         scale_factor = random_scale_factor(n_dim, std=scale_deviation)
-#     else:
-#         scale_factor = None
-#     if flip:
-#         flip_axis = random_flip_dimensions(n_dim)
-#     else:
-#         flip_axis = None
-#     data_list = list()
-#     for data_index in range(data.shape[0]):
-#         image = get_image(data[data_index], affine)
-#         data_list.append(resample_to_img(distort_image(image, flip_axis=flip_axis,
-#                                                        scale_factor=scale_factor), image,
-#                                          interpolation="continuous"))
-#     data = np.asarray(data_list)
-#     truth_image = get_image(truth, affine)
-#     truth_data = resample_to_img(distort_image(truth_image, flip_axis=flip_axis, scale_factor=scale_factor),
-#                                  truth_image, interpolation="nearest")
-#     return data, truth_data
+def augment_data(data, truth, affine, scale_deviation=None, flip=True):
+    n_dim = len(truth.shape)
+    if scale_deviation:
+        scale_factor = random_scale_factor(n_dim, std=scale_deviation)
+    else:
+        scale_factor = None
+    if flip:
+        flip_axis = random_flip_dimensions(n_dim)
+    else:
+        flip_axis = None
+    data_list = list()
+    for data_index in range(data.shape[0]):
+        image = get_image(data[data_index], affine)
+        data_list.append(resample_to_img(distort_image(image, flip_axis=flip_axis,
+                                                       scale_factor=scale_factor), image,
+                                         interpolation="continuous"))
+    data = np.asarray(data_list)
+    truth_image = get_image(truth, affine)
+    truth_data = resample_to_img(distort_image(truth_image, flip_axis=flip_axis, scale_factor=scale_factor),
+                                 truth_image, interpolation="nearest")
+    return data, truth_data
 
 
 # def get_image(data, affine, nib_class=nib.Nifti1Image):
