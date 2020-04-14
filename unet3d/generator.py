70d98634c3400ff28d2e8ef8bfa53858258ce57b
kjin <kjin@mit.edu>
2020-04-14 12:51:46 -0500

    print debugging generator.py

--- unet3d/generator.py
+++ unet3d/generator.py
@@ -51,8 +51,8 @@ def get_training_and_validation_generators(data_file_training, data_file_validat
     if not validation_batch_size:
         validation_batch_size = batch_size
 
-    training_list = list(range(15))
-    validation_list = list(range(15))
+    training_list = list(range(len(data_file_training['data'])))
+    training_list = list(range(len(data_file_training['truth'])))
     sys.stdout.write(str(training_list))
 
     training_generator = data_generator(data_file_training, training_list,
