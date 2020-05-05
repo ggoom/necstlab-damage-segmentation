44b010c4d33b07a875b780f2251307cc3fde8d4d
kjin <kjin@mit.edu>
2020-05-05 02:03:20 -0500

    augment for 3d debug augmented indices??

--- unet3d/generator.py
+++ unet3d/generator.py
@@ -153,9 +153,9 @@ def data_generator(data_file, index_list, batch_size=1, n_labels=1, labels=None,
         shuffle(x_list)
         shuffle(y_list)
 
-        # augmented_index_list = list(range(len(index_list) * 2))
-        # shuffle(augmented_index_list)
-        # index_list = augmented_index_list
+        augmented_index_list = list(range(len(index_list) * 2))
+        shuffle(augmented_index_list)
+        index_list = augmented_index_list
         while len(index_list) > 0:
             index = index_list.pop()
             x_data = x_list[index]
