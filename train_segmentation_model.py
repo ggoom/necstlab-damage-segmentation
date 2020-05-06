4815740ff8b21820b4cb05531a22b8b00c96b556
kjin <kjin@mit.edu>
2020-05-06 15:18:06 -0500

    trying without specifying steps per epoch, 1 epoch

--- train_segmentation_model.py
+++ train_segmentation_model.py
@@ -226,14 +226,14 @@ def train(gcp_bucket, config_file):
 
     results = compiled_model.fit(
         train_generator,
-        steps_per_epoch=n_train_steps if generator_type == '3D' else (len(train_generator) if augmentation_type ==
-                                                                      'necstlab' else train_config['data_augmentation']['bio_augmentation']['steps_per_epoch']),
+        # steps_per_epoch=n_train_steps if generator_type == '3D' else (len(train_generator) if augmentation_type ==
+        #                                                               'necstlab' else train_config['data_augmentation']['bio_augmentation']['steps_per_epoch']),
         workers=1,
         epochs=epochs,
         verbose=1,
         validation_data=validation_generator,
-        validation_steps=n_validation_steps if generator_type == '3D' else (len(
-            validation_generator) if augmentation_type == 'necstlab' else train_config['data_augmentation']['bio_augmentation']['validation_steps']),
+        # validation_steps=n_validation_steps if generator_type == '3D' else (len(
+        #     validation_generator) if augmentation_type == 'necstlab' else train_config['data_augmentation']['bio_augmentation']['validation_steps']),
         callbacks=[model_checkpoint_callback]
     )
 
